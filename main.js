0f3a36cba2b26a09dcd0301e01ed11991056c1de
Keslava-PL <kamdob1995@gmail.com>
2022-12-28 14:41:13 +0100

    Added css file, changes in html, changes on js file

--- main.js
+++ main.js
@@ -1,77 +1,64 @@
-//mani.js
-// KONWERTER TEMPERATUR
-
-
 var mainList = [];
-var ul = document.getElementById('checkList');
-var addButton = document.getElementById('addButton');
-var completedCheck = document.getElementById('completedCheck');
+var ul = document.getElementById("checkList");
+var addButton = document.getElementById("addButton");
+var completedCheck = document.getElementById("completedCheck");
 
-function checkedMarkedOn(){
+function checkedMarkedOn() {
   //console.log('in');
-  if(completedCheck.checked){
-    
-  var checkedBoxList = document.getElementsByClassName("checkListBox").value;
-  var checkedTable = [];
+  if (completedCheck.checked) {
+    let checkedBoxList = document.querySelectorAll(".checkListBox");
+    let checkedTable = [];
 
-  if(checkedBoxList.length>0)
-  {
-  for(var i=0; i<checkedBoxList.length;i++){
-    if(checkedBoxList[i].checked){
-      checkedTable.push(checkedBoxList[i]);
+    if (checkedBoxList.length > 0) {
+      for (var i = 0; i < checkedBoxList.length; i++) {
+        if (checkedBoxList[i].checked) {
+          checkedTable.push(checkedBoxList[i].name);
+        }
+      }
+      console.log("i",checkedTable);
+      showList(checkedTable);
+      
     }
-  }
-  showList(checkedTable.length>0?checkedTable:null);
-  }
-  }
-  else{
+  } else {
     showList(mainList);
   }
 }
 
-//showing checkbox list 
-function showList(lista){
+//showing checkbox list
+function showList(lista) {
   clearCheckedList();
 
+  if (lista.length > 0) {
+    for (var i = 0; i < mainList.length; i++) {
+      var li = document.createElement("li");
+      li.className = "checkedLiPoint";
+      var check = document.createElement("input");
 
-  if(lista.length >0){
-  for(var i = 0; i < mainList.length; i++){
+      check.type = "checkbox";
+      check.className = "checkListBox";
+      check.name = lista[i];
+      check.value = "value";
 
-    
-    var li = document.createElement('li');
-    li.className = "checkedLiPoint";
-    var check = document.createElement('input');
+      li.appendChild(document.createTextNode(lista[i]));
+      li.appendChild(check);
 
-    check.type = "checkbox";
-    check.className = "checkListBox"
-    check.name = "name";
-    check.value = "value"; 
-
-    li.appendChild(document.createTextNode(lista[i]));
-    li.appendChild(check);
-    
-    ul.appendChild(li);
+      ul.appendChild(li);
+    }
   }
-  }  
 }
 
 //clearing list and clearing mainList table;
-function clearCheckedList(){
-  
-  while(ul.hasChildNodes()){
+function clearCheckedList() {
+  while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
+    console.log("test");
   }
-
 }
 
-addButton.addEventListener('click', function (){
-  var texto = document.getElementById('textInput');
+addButton.addEventListener("click", function () {
+  const texto = document.getElementById("textInput");
   mainList.push(texto.value);
   showList(mainList);
 });
 
-completedCheck.addEventListener('change',checkedMarkedOn);
-
-
-
-
+completedCheck.addEventListener("change", checkedMarkedOn);
